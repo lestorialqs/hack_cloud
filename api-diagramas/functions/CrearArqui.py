1f3f02295ece2ec86315fe74cf1d78ba904d0c91
Henry Quispe Sutta <Henryqs>
2025-06-22 18:41:13 -0500

    wasf233

--- api-diagramas/functions/CrearArqui.py
+++ api-diagramas/functions/CrearArqui.py
@@ -5,6 +5,8 @@ def lambda_handler(event, context):
         "statusCode": 200,
         "body": {
             "dot_version": os.popen("/opt/python/bin/dot -V").read(),
-            "files_in_layer": os.listdir("/opt/python/bin/")[:10]
+            "files_in_layer": os.listdir("/opt/python/bin/")[:10],
+            "python_path": os.environ.get('PATH', ''),
+            "current_directory": os.listdir('.')
         }
     }
