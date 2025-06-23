6119c6838e661e3d84e47de82726829a9e3aaaf2
IsaacGamero <isaac.gamero@utec.edu.pe>
2025-06-22 19:04:49 -0500

    Update CrearJson.py

--- api-diagramas/functions/CrearJson.py
+++ api-diagramas/functions/CrearJson.py
@@ -3,7 +3,25 @@ import json
 from utils.json_to_mermaid import convert_json_to_mermaid
 
 def lambda_handler(event, context):
-
+    print(event)
+    # Entrada (json)
+    producto = event['body']
+    
+    # Inicio - Proteger el Lambda
+    token = event['headers']['token']
+    lambda_client = boto3.client('lambda')    
+    payload_string = '{ "token": "' + token +  '" }'
+    invoke_response = lambda_client.invoke(FunctionName="seguridad",
+                                           InvocationType='RequestResponse',
+                                           Payload = payload_string)
+    response = json.loads(invoke_response['Payload'].read())
+    print(response)
+    if response['statusCode'] == 403:
+        return {
+            'statusCode' : 403,
+            'status' : 'Forbidden - Acceso No Autorizado'
+        }
+    # Fin - Proteger el Lambda  
     
     try:
         body = event.get('body')
