a74d1081f004162888a3e147b2c79b6873ede511
Henry Quispe Sutta <Henryqs>
2025-06-22 20:08:19 -0500

    acutalizado

--- api-diagramas/functions/CrearArqui.py
+++ api-diagramas/functions/CrearArqui.py
@@ -1,16 +1,5 @@
-from diagrams import Diagram
-from diagrams.aws.compute import EC2
-import os
+
 
 def lambda_handler(event, context):
-    # Configura variables de entorno críticas
-    os.environ["PATH"] = f"/opt/bin:{os.environ['PATH']}"
-    
-    with Diagram("Demo", show=False, filename="/tmp/diagram"):
-        EC2("Web Server")
-    
-    with open("/tmp/diagram.dot", "r") as f:
-        return {
-            "statusCode": 200,
-            "body": f.read()  # Retorna el DOT para renderizar en frontend
-        }
+    print(event)
+    # Entrada (json)
